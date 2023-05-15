37cee3f2055e7421ccc8a7cf165e9e754dde334f
AngryBro <67253990+AngryBro@users.noreply.github.com>
2023-05-15 21:25:19 +1000

    Update lessonfiles.py

--- lessonfiles.py
+++ lessonfiles.py
@@ -1,12 +1,14 @@
 import requests
 
 class File(object):
-    def __init__(self, *args):
+    def __init__(self, args, nargs):
         #ссылка на репозиторий с заданиями, параметр1 = занятие, параметр2 = задача
-        repository = lambda lesson, task: f''
+        repository = lambda lesson, task: f'https://raw.githubusercontent.com/AngryBro/inf_ege_files/main/{lesson}/{task}.txt'
         #имя тестового файла по умолчанию
         default_test_file_name = 'test'
-        
+        self.test_data = None
+        if default_test_file_name in nargs:
+            self.test_data = nargs[default_test_file_name]
         lesson = None
         task = None
         self.file = None
@@ -15,6 +17,17 @@ class File(object):
         load_error = lambda path: print(f'Файл "{path}"\n  не удалось ни открыть ни загрузить')
         load_success = lambda: print(f'Файл загружен')
         load_begin = lambda url: print(f'Загрузка файла "{url}"')
+
+        def try_open_or_fetch(path, url = None):
+            if url == None:
+                url = path
+            try:
+                file = open(path)
+            except IOError:
+                fetch_data(url)
+            else:
+                self.file = file
+                self.fetched = False
         
         def fetch_data(url):
             load_begin(url)
@@ -41,28 +54,47 @@ class File(object):
 
         if len(args) == 1:
             path = args[0]
-            try:
-                file = open(path)
-            except IOError:
-                url = path
-                fetch_data(url)
+            if path == "":
+                if self.test_data == None:
+                    try_open_file(default_test_file_name)
+                else:
+                    self.file = self.test_data
             else:
-                self.file = file
-                self.fetched = False
+                try_open_or_fetch(path)
+                # try:
+                #     file = open(path)
+                # except IOError:
+                #     url = path
+                #     fetch_data(url)
+                # else:
+                #     self.file = file
+                #     self.fetched = False
         else:
             task = args[1]
             lesson = args[0]
             if task == "":
-                if len(args) > 2:
-                    try_open_file(args[2])
-                else:
+                if self.test_data == None:
                     try_open_file(default_test_file_name)
-            else:        
-                fetch_data(repository(lesson, task))
+                else:
+                    self.file = self.test_data
+            else:
+                try_open_or_fetch(task, repository(lesson, task))
+                # try:
+                #     file = open(task)
+                # except IOError:
+                #     url = repository(lesson, task)
+                #     fetch_data(url)
+                # else:
+                #     self.file = file
+                #     self.fetched = False       
 
     def read(self):
         if self.fetched == None:
-            return None
-        return self.file.text if self.fetched else self.file.read()
+            if self.test_data == None:
+                return None
+            return self.file
+        if self.fetched:
+            return self.file.text
+        return self.file.read()
 
-open_file = lambda *args: File(*args)
+open_file = lambda *args, **kwargs: File(args, kwargs)
